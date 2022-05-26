ca833a2bc094a9d22974f1d833ce9de85f32dc19
Warlock <internalmike@gmail.com>
2022-05-27 00:01:43 +0300

    freelist

--- src/freelist.rs
+++ src/freelist.rs
@@ -1,4 +1,4 @@
-//use crate::base::*;
+use crate::base::*;
 use core::alloc::{self, AllocError, Allocator};
 use core::cell::Cell;
 use core::ptr::{self, NonNull};
@@ -14,17 +14,11 @@ pub struct Freelist<A: Allocator> {
     layout: alloc::Layout,
 }
 
-/*
-unsafe impl Composable for Freelist {
-    unsafe fn has_allocated(&self, ptr: NonNull<u8>, _layout: alloc::Layout) -> bool {
-        if let Some(offset) = (ptr.as_ptr() as usize).checked_sub(self.buf_ptr.as_ptr() as usize) {
-            offset < self.buf_len && self.buf_ptr.as_ptr().offset(offset as isize) == ptr.as_ptr()
-        } else {
-            false
-        }
+unsafe impl<A: Composable> Composable for Freelist<A> {
+    unsafe fn has_allocated(&self, ptr: NonNull<u8>, layout: alloc::Layout) -> bool {
+        self.base.has_allocated(ptr, layout)
     }
 }
-*/
 
 unsafe impl<A: Allocator> Allocator for Freelist<A> {
     fn allocate(&self, layout: alloc::Layout) -> Result<NonNull<[u8]>, AllocError> {
@@ -48,27 +42,13 @@ unsafe impl<A: Allocator> Allocator for Freelist<A> {
         self.list.set(Some(NonNull::slice_from_raw_parts(ptr, layout.size())));
     }
 
-    /*
     unsafe fn grow(
         &self, 
         ptr: NonNull<u8>, 
         old_layout: alloc::Layout, 
         new_layout: alloc::Layout
     ) -> Result<NonNull<[u8]>, AllocError> {
-        if
-            ptr.as_ptr().offset(old_layout.size() as isize) ==
-            self.buf_ptr.as_ptr().offset(self.allocated.get() as isize)
-        {
-            self.allocated.set(self.allocated.get() - old_layout.size());
-            if let Ok(block) = self.allocate(new_layout) {
-                Ok(block)
-            } else {
-                self.allocated.set(self.allocated.get() + old_layout.size());
-                Err(AllocError)
-            }
-        } else {
-            Err(AllocError)
-        }
+        self.base.grow(ptr, old_layout, new_layout)
     }
 
     unsafe fn shrink(
@@ -77,24 +57,6 @@ unsafe impl<A: Allocator> Allocator for Freelist<A> {
         old_layout: alloc::Layout, 
         new_layout: alloc::Layout
     ) -> Result<NonNull<[u8]>, AllocError> {
-        if
-            ptr.as_ptr().offset(old_layout.size() as isize) ==
-            self.buf_ptr.as_ptr().offset(self.allocated.get() as isize)
-        {
-            self.allocated.set(self.allocated.get() - old_layout.size());
-            if let Ok(block) = self.allocate(new_layout) {
-                Ok(block)
-            } else {
-                self.allocated.set(self.allocated.get() + old_layout.size());
-                Err(AllocError)
-            }
-        } else {
-            if new_layout.align() > old_layout.align() {
-                Err(AllocError)
-            } else {
-                Ok(NonNull::slice_from_raw_parts(ptr, old_layout.size()))
-            }
-        }
+        self.base.shrink(ptr, old_layout, new_layout)
     }
-    */
 }
