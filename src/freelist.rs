5d20381cd8451ddfa7e4ef9efa4869cf372d68ca
Warlock <internalmike@gmail.com>
2022-05-27 02:06:39 +0300

    low_bound

--- src/freelist.rs
+++ src/freelist.rs
@@ -12,6 +12,30 @@ pub struct Freelist<A: Allocator> {
     base: A,
     list: Cell<Option<NonNull<[u8]>>>,
     layout: alloc::Layout,
+    low_bound: alloc::Layout,
+}
+
+impl<A: Composable> Freelist<A> {
+    /// # Safety
+    ///
+    /// Arguments should satisfy
+    /// `low_bound.size() <= layout.size() && low_bound.align() <= layout.align()`.
+    pub unsafe fn new_unchecked(base: A, layout: alloc::Layout, low_bound: alloc::Layout) -> Self {
+        Freelist { base, list: Cell::new(None), layout, low_bound }
+    }
+
+    pub fn new(base: A, layout: alloc::Layout, low_bound: alloc::Layout) -> Self {
+        let low_bound = unsafe { alloc::Layout::from_size_align_unchecked(
+            min(low_bound.size(), layout.size()),
+            min(low_bound.align(), layout.align()),
+        ) };
+        Freelist { base, list: Cell::new(None), layout, low_bound }
+    }
+
+    fn manage(&self, layout: alloc::Layout) -> bool {
+        layout.size() in self.low_bound.size() ..= self.layout.size() &&
+        layout.align() in self.low_bound.align() ..= self.layout.size()
+    }
 }
 
 unsafe impl<A: Composable> Composable for Freelist<A> {
@@ -22,7 +46,7 @@ unsafe impl<A: Composable> Composable for Freelist<A> {
 
 unsafe impl<A: Allocator> Allocator for Freelist<A> {
     fn allocate(&self, layout: alloc::Layout) -> Result<NonNull<[u8]>, AllocError> {
-        if layout != self.layout {
+        if !self.manage(layout) {
             return self.base.allocate(layout);
         }
         if let Some(list) = self.list.get() {
@@ -30,16 +54,16 @@ unsafe impl<A: Allocator> Allocator for Freelist<A> {
             self.list.set(next);
             Ok(list)
         } else {
-            self.base.allocate(layout)
+            self.base.allocate(self.layout)
         }
     }
 
     unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: alloc::Layout) {
-        if layout != self.layout {
+        if !self.manage(layout) {
             return self.base.deallocate(ptr, layout);
         }
         ptr::write(ptr.as_ptr() as *mut Node, Node { next: self.list.get() });
-        self.list.set(Some(NonNull::slice_from_raw_parts(ptr, layout.size())));
+        self.list.set(Some(NonNull::slice_from_raw_parts(ptr, self.layout.size())));
     }
 
     unsafe fn grow(
@@ -48,6 +72,7 @@ unsafe impl<A: Allocator> Allocator for Freelist<A> {
         old_layout: alloc::Layout, 
         new_layout: alloc::Layout
     ) -> Result<NonNull<[u8]>, AllocError> {
+        let old_layout = if self.manage(old_layout) { self.layout } else { old_layout };
         self.base.grow(ptr, old_layout, new_layout)
     }
 
@@ -57,6 +82,14 @@ unsafe impl<A: Allocator> Allocator for Freelist<A> {
         old_layout: alloc::Layout, 
         new_layout: alloc::Layout
     ) -> Result<NonNull<[u8]>, AllocError> {
+        let old_layout = if self.manage(old_layout) {
+            if self.manage(new_layout) {
+                return Ok(NonNull::slice_from_raw_parts(ptr, self.layout.size()));
+            }
+            self.layout
+        } else {
+            old_layout
+        };
         self.base.shrink(ptr, old_layout, new_layout)
     }
 }
