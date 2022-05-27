099e8871bab582c09c8804aa363268abb0f2926d
Warlock <internalmike@gmail.com>
2022-05-27 16:22:12 +0300

    optional limit

--- src/freelist.rs
+++ src/freelist.rs
@@ -10,6 +10,75 @@ pub const MIN_LAYOUT_ALIGN: usize = align_of::<Node>();
 
 /// # Safety
 ///
+/// This trait cannot be implemented outside of this module.
+pub unsafe trait LimitParam {
+    #[doc(hidden)]
+    unsafe fn limit_reached(&self) -> bool;
+
+    #[doc(hidden)]
+    unsafe fn dec_list_len(&self);
+
+    #[doc(hidden)]
+    unsafe fn inc_list_len(&self);
+}
+
+pub struct NoLimit;
+
+impl const Default for NoLimit {
+    fn default() -> Self { NoLimit }
+}
+
+unsafe impl LimitParam for NoLimit {
+    unsafe fn limit_reached(&self) -> bool { false }
+
+    unsafe fn dec_list_len(&self) { }
+
+    unsafe fn inc_list_len(&self) { }
+}
+
+pub struct FixedCtLimit<const LIMIT: usize> {
+    list_len: Cell<usize>,
+}
+
+impl<const LIMIT: usize> const Default for FixedCtLimit<LIMIT> {
+    fn default() -> Self { FixedCtLimit { list_len: Cell::new(0) } }
+}
+
+unsafe impl<const LIMIT: usize> LimitParam for FixedCtLimit<LIMIT> {
+    unsafe fn limit_reached(&self) -> bool {
+        self.list_len.get() == LIMIT
+    }
+
+    unsafe fn dec_list_len(&self) {
+        self.list_len.set(self.list_len.get() - 1);
+    }
+
+    unsafe fn inc_list_len(&self) {
+        self.list_len.set(self.list_len.get() + 1);
+    }
+}
+
+pub struct FixedRtLimit {
+    limit: usize,
+    list_len: Cell<usize>,
+}
+
+unsafe impl LimitParam for FixedRtLimit {
+    unsafe fn limit_reached(&self) -> bool {
+        self.list_len.get() == self.limit
+    }
+
+    unsafe fn dec_list_len(&self) {
+        self.list_len.set(self.list_len.get() - 1);
+    }
+
+    unsafe fn inc_list_len(&self) {
+        self.list_len.set(self.list_len.get() + 1);
+    }
+}
+
+/// # Safety
+///
 /// All methods should return constants, i.e. same values on every call.
 ///
 /// Returned values should satisfy
@@ -17,9 +86,10 @@ pub const MIN_LAYOUT_ALIGN: usize = align_of::<Node>();
 /// and
 /// `layout.size() >= MIN_LAYOUT_SIZE && layout.align() >= MIN_LAYOUT_ALIGN`.
 pub unsafe trait Params {
+    type Limit: LimitParam;
     fn layout(&self) -> alloc::Layout;
     fn tolerance(&self) -> alloc::Layout;
-    fn top(&self) -> usize;
+    fn limit(&self) -> &Self::Limit;
 }
 
 pub struct CtParams<
@@ -27,8 +97,10 @@ pub struct CtParams<
     const LAYOUT_ALIGN: usize,
     const TOLERANCE_SIZE: usize,
     const TOLERANCE_ALIGN: usize,
-    const TOP: usize,
->(());
+    Limit: LimitParam,
+> {
+    limit: Limit,
+}
 
 const fn is_power_of_two(x: usize) -> bool {
     x != 0 && (x & (x - 1)) == 0
@@ -39,9 +111,9 @@ impl<
     const LAYOUT_ALIGN: usize,
     const TOLERANCE_SIZE: usize,
     const TOLERANCE_ALIGN: usize,
-    const TOP: usize,
-> CtParams<LAYOUT_SIZE, LAYOUT_ALIGN, TOLERANCE_SIZE, TOLERANCE_ALIGN, TOP> {
-    pub const fn new() -> Self {
+    Limit: LimitParam,
+> CtParams<LAYOUT_SIZE, LAYOUT_ALIGN, TOLERANCE_SIZE, TOLERANCE_ALIGN, Limit> {
+    pub const fn new(limit: Limit) -> Self {
         assert!(LAYOUT_SIZE <= isize::MAX as usize);
         assert!(LAYOUT_ALIGN <= isize::MAX as usize);
         assert!(TOLERANCE_SIZE <= isize::MAX as usize);
@@ -51,7 +123,7 @@ impl<
         assert!(is_power_of_two(LAYOUT_ALIGN) && is_power_of_two(TOLERANCE_ALIGN));
         assert!(TOLERANCE_SIZE <= LAYOUT_SIZE && TOLERANCE_ALIGN <= LAYOUT_ALIGN);
         assert!(LAYOUT_SIZE >= MIN_LAYOUT_SIZE && LAYOUT_ALIGN >= MIN_LAYOUT_ALIGN);
-        CtParams(())
+        CtParams { limit }
     }
 }
 
@@ -60,9 +132,9 @@ impl<
     const LAYOUT_ALIGN: usize,
     const TOLERANCE_SIZE: usize,
     const TOLERANCE_ALIGN: usize,
-    const TOP: usize
-> const Default for CtParams<LAYOUT_SIZE, LAYOUT_ALIGN, TOLERANCE_SIZE, TOLERANCE_ALIGN, TOP> {
-    fn default() -> Self { Self::new() }
+    Limit: LimitParam + ~const Default,
+> const Default for CtParams<LAYOUT_SIZE, LAYOUT_ALIGN, TOLERANCE_SIZE, TOLERANCE_ALIGN, Limit> {
+    fn default() -> Self { Self::new(Limit::default()) }
 }
 
 unsafe impl<
@@ -70,8 +142,10 @@ unsafe impl<
     const LAYOUT_ALIGN: usize,
     const TOLERANCE_SIZE: usize,
     const TOLERANCE_ALIGN: usize,
-    const TOP: usize,
-> const Params for CtParams<LAYOUT_SIZE, LAYOUT_ALIGN, TOLERANCE_SIZE, TOLERANCE_ALIGN, TOP> {
+    Limit: LimitParam,
+> const Params for CtParams<LAYOUT_SIZE, LAYOUT_ALIGN, TOLERANCE_SIZE, TOLERANCE_ALIGN, Limit> {
+    type Limit = Limit;
+
     fn layout(&self) -> alloc::Layout {
         unsafe { alloc::Layout::from_size_align_unchecked(LAYOUT_SIZE, LAYOUT_ALIGN) }
     }
@@ -80,39 +154,41 @@ unsafe impl<
         unsafe { alloc::Layout::from_size_align_unchecked(TOLERANCE_SIZE, TOLERANCE_ALIGN) }
     }
 
-    fn top(&self) -> usize { TOP }
+    fn limit(&self) -> &Limit { &self.limit }
 }
 
-pub struct RtParams {
+pub struct RtParams<Limit: LimitParam> {
     layout: alloc::Layout,
     tolerance: alloc::Layout,
-    top: usize,
+    limit: Limit,
 }
 
-impl RtParams {
+impl<Limit: LimitParam> RtParams<Limit> {
     /// # Safety
     ///
     /// Arguments should satisfy
     /// `tolerance.size() <= layout.size() && tolerance.align() <= layout.align()`,
     /// and
     /// `layout.size() >= MIN_LAYOUT_SIZE && layout.align() >= MIN_LAYOUT_ALIGN`.
-    pub unsafe fn new_unchecked(layout: alloc::Layout, tolerance: alloc::Layout, top: usize) -> Self {
-        RtParams { layout, tolerance, top }
+    pub unsafe fn new_unchecked(layout: alloc::Layout, tolerance: alloc::Layout, limit: Limit) -> Self {
+        RtParams { layout, tolerance, limit }
     }
 
-    pub fn new(layout: alloc::Layout, tolerance: alloc::Layout, top: usize) -> Self {
+    pub fn new(layout: alloc::Layout, tolerance: alloc::Layout, limit: Limit) -> Self {
         assert!(tolerance.size() <= layout.size() && tolerance.align() <= layout.align());
         assert!(layout.size() >= MIN_LAYOUT_SIZE && layout.align() >= MIN_LAYOUT_ALIGN);
-        unsafe { RtParams::new_unchecked(layout, tolerance, top) }
+        unsafe { RtParams::new_unchecked(layout, tolerance, limit) }
     }
 }
 
-unsafe impl Params for RtParams {
+unsafe impl<Limit: LimitParam> Params for RtParams<Limit> {
+    type Limit = Limit;
+
     fn layout(&self) -> alloc::Layout { self.layout }
 
     fn tolerance(&self) -> alloc::Layout { self.tolerance }
 
-    fn top(&self) -> usize { self.top }
+    fn limit(&self) -> &Limit { &self.limit }
 }
 
 #[derive(Clone, Copy)]
@@ -123,13 +199,12 @@ struct Node {
 pub struct Freelist<P: Params, A: Allocator> {
     base: A,
     list: Cell<Node>,
-    list_len: Cell<usize>,
     params: P,
 }
 
 impl<P: Params, A: Allocator> Freelist<P, A> {
     pub fn new(params: P, base: A) -> Self {
-        Freelist { base, list: Cell::new(Node { next: None }), list_len: Cell::new(0), params }
+        Freelist { base, list: Cell::new(Node { next: None }), params }
     }
 
     fn manage(&self, layout: alloc::Layout) -> bool {
@@ -153,7 +228,7 @@ unsafe impl<P: Params, A: Allocator> Allocator for Freelist<P, A> {
         if let Some(list) = self.list.get().next {
             let next = unsafe { ptr::read(list.as_ptr() as *const Node) }.next;
             self.list.set(Node { next });
-            self.list_len.set(self.list_len.get() - 1);
+            unsafe { self.params.limit().dec_list_len(); }
             Ok(NonNull::slice_from_raw_parts(list, self.params.layout().size()))
         } else {
             self.base.allocate(self.params.layout())
@@ -161,12 +236,12 @@ unsafe impl<P: Params, A: Allocator> Allocator for Freelist<P, A> {
     }
 
     unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: alloc::Layout) {
-        if self.list_len.get() == self.params.top() || !self.manage(layout) {
+        if self.params.limit().limit_reached() || !self.manage(layout) {
             return self.base.deallocate(ptr, layout);
         }
         ptr::write(ptr.as_ptr() as *mut Node, self.list.get());
         self.list.set(Node { next: Some(ptr) });
-        self.list_len.set(self.list_len.get() + 1);
+        self.params.limit().inc_list_len();
     }
 
     unsafe fn grow(
