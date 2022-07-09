52d4aa7bf60143e18b109449ff08b4a5e2ebc1ee
Warlock <internalmike@gmail.com>
2022-07-10 00:46:16 +0300

    f

--- src/logging.rs
+++ src/logging.rs
@@ -2,42 +2,34 @@ use crate::base::*;
 use core::alloc::{self, AllocError, Allocator};
 use core::ptr::NonNull;
 
-pub struct Logging<A: Allocator> {
-    base: A,
-}
+pub struct Logging<A: Allocator>(pub A);
 
 unsafe impl<A: NonUnwinding> NonUnwinding for Logging<A> { }
 
-impl<A: Allocator> Logging<A> {
-    pub const fn new(base: A) -> Self {
-        Logging { base }
-    }
-}
-
 unsafe impl<A: Fallbackable> Fallbackable for Logging<A> {
     unsafe fn has_allocated(&self, ptr: NonNull<u8>, layout: alloc::Layout) -> bool {
-        self.base.has_allocated(ptr, layout)
+        self.0.has_allocated(ptr, layout)
     }
 
     fn allows_fallback(&self, layout: alloc::Layout) -> bool {
-        self.base.allows_fallback(layout)
+        self.0.allows_fallback(layout)
     }
 }
 
 unsafe impl<A: Allocator> Allocator for Logging<A> {
     fn allocate(&self, layout: alloc::Layout) -> Result<NonNull<[u8]>, AllocError> {
         eprintln!("allocate: {:?}", layout);
-        self.base.allocate(layout)
+        self.0.allocate(layout)
     }
 
     fn allocate_zeroed(&self, layout: alloc::Layout) -> Result<NonNull<[u8]>, AllocError> {
         eprintln!("allocate_zeroed: {:?}", layout);
-        self.base.allocate_zeroed(layout)
+        self.0.allocate_zeroed(layout)
     }
 
     unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: alloc::Layout) {
         eprintln!("deallocate: {:?}", layout);
-        self.base.deallocate(ptr, layout)
+        self.0.deallocate(ptr, layout)
     }
 
     unsafe fn grow(
@@ -47,7 +39,7 @@ unsafe impl<A: Allocator> Allocator for Logging<A> {
         new_layout: alloc::Layout
     ) -> Result<NonNull<[u8]>, AllocError> {
         eprintln!("grow: {:?} -> {:?}", old_layout, new_layout);
-        self.base.grow(ptr, old_layout, new_layout)
+        self.0.grow(ptr, old_layout, new_layout)
     }
 
     unsafe fn grow_zeroed(
@@ -57,7 +49,7 @@ unsafe impl<A: Allocator> Allocator for Logging<A> {
         new_layout: alloc::Layout
     ) -> Result<NonNull<[u8]>, AllocError> {
         eprintln!("grow_zeroed: {:?} -> {:?}", old_layout, new_layout);
-        self.base.grow_zeroed(ptr, old_layout, new_layout)
+        self.0.grow_zeroed(ptr, old_layout, new_layout)
     }
 
     unsafe fn shrink(
@@ -67,6 +59,6 @@ unsafe impl<A: Allocator> Allocator for Logging<A> {
         new_layout: alloc::Layout
     ) -> Result<NonNull<[u8]>, AllocError> {
         eprintln!("shrink: {:?} -> {:?}", old_layout, new_layout);
-        self.base.shrink(ptr, old_layout, new_layout)
+        self.0.shrink(ptr, old_layout, new_layout)
     }
 }
