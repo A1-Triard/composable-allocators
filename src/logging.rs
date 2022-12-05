9ba1650e36307f1d1e66a0fef7f41539ef6aebbc
Warlock <internalmike@gmail.com>
2022-12-05 12:47:56 +0300

    clippy

--- src/logging.rs
+++ src/logging.rs
@@ -18,17 +18,17 @@ unsafe impl<A: Fallbackable> Fallbackable for Logging<A> {
 
 unsafe impl<A: Allocator> Allocator for Logging<A> {
     fn allocate(&self, layout: alloc::Layout) -> Result<NonNull<[u8]>, AllocError> {
-        eprintln!("allocate: {:?}", layout);
+        eprintln!("allocate: {layout:?}");
         self.0.allocate(layout)
     }
 
     fn allocate_zeroed(&self, layout: alloc::Layout) -> Result<NonNull<[u8]>, AllocError> {
-        eprintln!("allocate_zeroed: {:?}", layout);
+        eprintln!("allocate_zeroed: {layout:?}");
         self.0.allocate_zeroed(layout)
     }
 
     unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: alloc::Layout) {
-        eprintln!("deallocate: {:?}", layout);
+        eprintln!("deallocate: {layout:?}");
         self.0.deallocate(ptr, layout)
     }
 
@@ -38,7 +38,7 @@ unsafe impl<A: Allocator> Allocator for Logging<A> {
         old_layout: alloc::Layout, 
         new_layout: alloc::Layout
     ) -> Result<NonNull<[u8]>, AllocError> {
-        eprintln!("grow: {:?} -> {:?}", old_layout, new_layout);
+        eprintln!("grow: {old_layout:?} -> {new_layout:?}");
         self.0.grow(ptr, old_layout, new_layout)
     }
 
@@ -48,7 +48,7 @@ unsafe impl<A: Allocator> Allocator for Logging<A> {
         old_layout: alloc::Layout, 
         new_layout: alloc::Layout
     ) -> Result<NonNull<[u8]>, AllocError> {
-        eprintln!("grow_zeroed: {:?} -> {:?}", old_layout, new_layout);
+        eprintln!("grow_zeroed: {old_layout:?} -> {new_layout:?}");
         self.0.grow_zeroed(ptr, old_layout, new_layout)
     }
 
@@ -58,7 +58,7 @@ unsafe impl<A: Allocator> Allocator for Logging<A> {
         old_layout: alloc::Layout, 
         new_layout: alloc::Layout
     ) -> Result<NonNull<[u8]>, AllocError> {
-        eprintln!("shrink: {:?} -> {:?}", old_layout, new_layout);
+        eprintln!("shrink: {old_layout:?} -> {new_layout:?}");
         self.0.shrink(ptr, old_layout, new_layout)
     }
 }
