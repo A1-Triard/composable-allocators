0ea72eea15351419e199f60276926ca754db982f
Master <internalmike@gmail.com>
2025-12-10 22:47:13 +0300

    update

--- ensure_no_std/src/main.rs
+++ ensure_no_std/src/main.rs
@@ -1,16 +1,16 @@
 #![feature(allocator_api)]
 #![feature(iter_collect_into)]
-#![feature(start)]
 
 #![deny(warnings)]
 
 #![no_std]
+#![no_main]
 
 extern crate alloc;
 
 #[cfg(windows)]
 #[link(name="msvcrt")]
-extern { }
+extern "C" { }
 
 mod no_std {
     use composable_allocators::{AsGlobal, System};
@@ -30,9 +30,10 @@ use alloc::vec::Vec;
 use composable_allocators::Global;
 use composable_allocators::fallbacked::Fallbacked;
 use composable_allocators::stacked::{self};
+use core::ffi::{c_char, c_int};
 
-#[start]
-pub fn main(_argc: isize, _argv: *const *const u8) -> isize {
+#[unsafe(no_mangle)]
+extern "C" fn main(_argc: c_int, _argv: *mut *mut c_char) -> c_int {
     stacked::with_size::<256, _>(|stacked| {
         let mut vec = Vec::new_in(Fallbacked(stacked, Global));
         [0u8, 1, 2, 3].iter().copied().collect_into(&mut vec);
